33984a4281f187bf6ecb33cd916403d9a80fc252
musai7 <musaishankar307@gmail.com>
2021-11-21 23:02:29 +0530

    added : bubble sorting

--- src/com/bridgeit/algorithmProblem/Main.java
+++ src/com/bridgeit/algorithmProblem/Main.java
@@ -8,28 +8,40 @@ public class Main {
 		String string = "An,algorithm,is,a,set,of,instructions,for,solving,logical,and,mathematical,problems";
 		String[] stringArray = string.split(",");
 
-		
 		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("ente 1 : for search \n enter 2 : for insertion sorting ");
-		int num = scanner.nextInt();
 
-		switch (num) {
-		case 1:
-			System.out.println("enter a word to search");
-			String str = scanner.next();
-			int result = SortingAlgorithm.binarySearch(stringArray, str);
+		while (true) {
+			System.out.println("enter 0 : for exit \n ente 1 : for search \n enter 2 : for insertion sorting "
+					+ "\n enter 3 : for bubbleSort");
+			int num = scanner.nextInt();
 
-			if (result == -1)
-				System.out.println("Element not present");
-			else
-				System.out.println("Element found at " + "index " + result);
-			break;
-		case 2 :
-			SortingAlgorithm.insertionSort(stringArray);
-			break;
-		default :
-			System.out.println("enter valid case number ");
+			switch (num) {
+
+			case 0:
+				System.out.println("--------END--------");
+				break;
+			case 1:
+				System.out.println("enter a word to search");
+				String str = scanner.next();
+				int result = SortingAlgorithm.binarySearch(stringArray, str);
+
+				if (result == -1)
+					System.out.println("Element not present");
+				else
+					System.out.println("Element found at " + "index " + result);
+				break;
+			case 2:
+				SortingAlgorithm.insertionSort(stringArray);
+				break;
+			case 3:
+				SortingAlgorithm.bubbleSorting(stringArray);
+				break;
+			default:
+				System.out.println("enter valid case number ");
+			}
+			if (num == 0) {
+				break;
+			}
 		}
 	}
 }
--- src/com/bridgeit/algorithmProblem/SortingAlgorithm.java
+++ src/com/bridgeit/algorithmProblem/SortingAlgorithm.java
@@ -38,4 +38,18 @@ public class SortingAlgorithm {
 		}
 		System.out.println();
 	}
+
+	public static void bubbleSorting(String[] stringArray) {
+
+		for (int i = 0; i < stringArray.length - 1; i++) {
+			for (int j = 0; j < stringArray.length - i - 1; j++) {
+				if (stringArray[j].compareTo(stringArray[j + 1]) > 0) {
+					String temp = stringArray[j];
+					stringArray[j] = stringArray[j + 1];
+					stringArray[j + 1] = temp;
+				}
+			}
+		}
+		printArray(stringArray);
+	}
 }
